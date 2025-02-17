e4919115dea34f64d788b962154ab14767a3fa88
jmg24a <jmg24a@gmail.com>
2025-02-16 23:30:26 -0400

    login css

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,16 +1,44 @@
 "use client"
 import Image from 'next/image';
+import { SetStateAction, useState } from "react";
+
 
 const Login = () => {
     // const myAppApi = process.env.NEXT_PUBLIC_API_URL;
+    const [flat, setFlat] = useState<boolean>(false);
+
+
+    const handlerClick = () => {
+        setFlat(!flat);
+    }
     
-  return (
-    <div className='login'>
-        <p>
-            <Image src="/login.svg" alt="" width={30} height={30}/>
-        </p>
-    </div>
-  );
+    if(!flat){
+        return (
+          <div className='login--button' onClick={handlerClick}>
+              <Image src="/login.svg" alt="" width={30} height={30}/>
+          </div>
+        );
+    }else{
+        return (
+            <div className='login_container'>
+                <div className='login_content'>
+                    <p>Login</p>
+                    <input type="text" className='login_content--input'/>
+                    <input type="password" className='login_content--input'/>
+                    <button
+                        className="close-btn--send"
+                        onClick={handlerClick}
+                    >Enviar
+                    </button>
+                    <button 
+                        className="close-btn"
+                        onClick={handlerClick}
+                    >Cerrar
+                    </button>
+                </div>
+            </div>
+        )
+    }
 };
 
 export default Login;
