d27e80d864965cc8c1b601f72dc96729d1c2f3d7
jmg24a <jmg24a@gmail.com>
2025-05-28 12:30:05 -0400

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,56 +5,11 @@ import Products from "../component/Products"
 
 type Props = {
   searchParams: {
-    category: string;
+    category?: string;
   };
 };
 
-const getDollar  = async () => {
-    const response = await fetch("https://pydolarve.org/api/v1/dollar?page=bcv&format_date=default&rounded_price=true",{
-        method: 'GET', 
-        headers: {
-            'Content-Type': 'application/json'
-          },
-    });
-    const data = await response.json();
-    return data;
-}
-
-const getGlobalPrice  = async () => {
-    const secretKey = process.env.API_SECRET_KEY;
-    const backApi = process.env.NEXT_PUBLIC_API_BACK_URL;
-    const response = await fetch(`${backApi}/api/v1/products?category=global`,{
-        method: 'GET', 
-        headers: {
-            'Content-Type': 'application/json',
-            'api': `${secretKey}`, 
-          },
-    });
-    const data = await response.json();
-    return data;
-}
-
-const getProducts  = async (category: string) => {
-    const secretKey = process.env.API_SECRET_KEY;
-    const backApi = process.env.NEXT_PUBLIC_API_BACK_URL;
-    const response = await fetch(`${backApi}/api/v1/products?category=${category}`,{
-        method: 'GET', 
-        headers: {
-            'Content-Type': 'application/json',
-            'api': `${secretKey}`, 
-          },
-    });
-    const data = await response.json();
-    return data;
-}
-
-export default async function HomePage({ searchParams }: Props) {
-
-    const [products, dollar, price] = await Promise.all([
-      getProducts(searchParams.category),
-      getDollar(),
-      getGlobalPrice()
-    ]);
+export default function HomePage({ searchParams }: Props) {
 
   return (
     <div>
@@ -73,9 +28,7 @@ export default async function HomePage({ searchParams }: Props) {
       <Navbar/>
 
       {/* Rejilla de productos */}
-      <Products
-        products={products}
-      />
+      <Products />
     </div>
   );
 }
--- src/component/Products.tsx
+++ src/component/Products.tsx
@@ -1,5 +1,4 @@
 "use client"
-import Link from 'next/link';
 import styles from "../app/Home.module.css";
 import Image from 'next/image';
 
@@ -9,27 +8,72 @@ const products2 = Array(4).fill({
   imageUrl: "/sofa.jpg",
 });
 
-interface typeProducts {
-    products: [
-        {
-            category: string,
-            createdAt: string,
-            description: string,
-            id: string,
-            image: string,
-            name: string,
-            price: string,
-            size: string
-        }
-    ]
+interface typeProduct {
+    category: string,
+    createdAt: string,
+    description: string,
+    id: string,
+    image: string,
+    name: string,
+    price: string,
+    size: string
 }
 
-const Products = ({products}:typeProducts) => {
+interface typeProps {
+    category: string
+}
+
+const getDollar  = async () => {
+    const response = await fetch("https://pydolarve.org/api/v1/dollar?page=bcv&format_date=default&rounded_price=true",{
+        method: 'GET', 
+        headers: {
+            'Content-Type': 'application/json'
+          },
+    });
+    const data = await response.json();
+    return data;
+}
+
+const getGlobalPrice  = async () => {
+    const secretKey = process.env.API_SECRET_KEY;
+    const backApi = process.env.NEXT_PUBLIC_API_BACK_URL;
+    const response = await fetch(`${backApi}/api/v1/products?category=global`,{
+        method: 'GET', 
+        headers: {
+            'Content-Type': 'application/json',
+            'api': `${secretKey}`, 
+          },
+    });
+    const data = await response.json();
+    return data;
+}
+
+const getProducts  = async ({category}: typeProps) => {
+    const secretKey = process.env.API_SECRET_KEY;
+    const backApi = process.env.NEXT_PUBLIC_API_BACK_URL;
+    const response = await fetch(`${backApi}/api/v1/products?category=${category}`,{
+        method: 'GET', 
+        headers: {
+            'Content-Type': 'application/json',
+            'api': `${secretKey}`, 
+          },
+    });
+    const data = await response.json();
+    return data;
+}
+
+const Products = async ({category}:any) => {
     const myAppApi = process.env.NEXT_PUBLIC_API_URL;
-    console.log('products', products)
+
+    const [products, dollar, price] = await Promise.all([
+      getProducts(category),
+      getDollar(),
+      getGlobalPrice()
+    ]);
+
   return (
       <div className={styles.productsGrid}>
-        {products.map((product, key) => (
+        {products.map((product: typeProduct, key:any) => (
           <div key={key} className={styles.productCard}>
             <Image
               src={product.image}
